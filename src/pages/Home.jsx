113819101a24fcb327704c56c6f4779896e061e8
Churchillk <churchilkodhiambo@gmail.com>
2026-02-17 11:21:29 +0300

    Home1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import Layout from '../components/layouts/Layout';
 import Hero from '../components/home/Hero';
 import AboutSection from '../components/Home/AboutSection';
-import WhyChooseUs from '../components/home/WhyChooseUs';
+import WhyChooseUs from '../components/Home/WhyChooseUs';
 import Gallery from '../components/home/Gallery';
-import PackageSlider from '../components/home/PackageSlider';
+import PackageSlider from '../components/Home/PackageSlider';
 import Testimonials from '../components/Home/Testimonials';
 import '../styles/globals.css';
 
