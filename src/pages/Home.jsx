8d649f1a262245bb21336217c6f7e745a6054728
Churchillk <churchilkodhiambo@gmail.com>
2026-02-17 11:54:08 +0300

    Home1 ending 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Layout from '/src/components/layouts/Layout';
-import Hero from '/src/components/Home/Hero.jsx';
-import AboutSection from '../components/Home/AboutSection';
-import WhyChooseUs from '../components/Home/WhyChooseUs';
-import Gallery from '../components/Home/Gallery';
-import PackageSlider from '../components/Home/PackageSlider';
-import Testimonials from '../components/Home/Testimonials';
+import Layout from '../components/layouts/Layout';
+import Hero from '../components/home/Hero';
+import AboutSection from '../components/home/AboutSection';
+import WhyChooseUs from '../components/home/WhyChooseUs';
+import Gallery from '../components/home/Gallery';
+import PackageSlider from '../components/home/PackageSlider';
+import Testimonials from '../components/home/Testimonials';
 import '../styles/globals.css';
 
 const Home = () => {
